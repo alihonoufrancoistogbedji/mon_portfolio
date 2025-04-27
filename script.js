04a522460e0adf2d7b17920a4ede7e79db18f60a
François Togbedji <alihonoufrancoistogbedji@gmail.com>
2025-04-27 03:38:38 +0100

    Mon premier portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+lottie.loadAnimation({
+    container: document.getElementById('animationContainer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'D:\PORTFOLIO\essai.json' // chemin du fichier
+  });
+  
