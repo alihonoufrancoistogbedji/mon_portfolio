741518988497b9d0b8ac7042fcff0622dcfa7de1
François Togbedji <alihonoufrancoistogbedji@gmail.com>
2025-04-27 04:15:56 +0100

    Déplacement de index.html à la racine

--- script.js
+++
@@ -1,8 +0,0 @@
-lottie.loadAnimation({
-    container: document.getElementById('animationContainer'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'D:\PORTFOLIO\essai.json' // chemin du fichier
-  });
-  
